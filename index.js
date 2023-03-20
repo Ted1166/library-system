be675e973b2619e91de15ac651f160a71b97cc40
master <ogolated00@gmail.com>
2023-03-20 13:24:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function showHomePage() {
+    var homePage = document.getElementById("homePage");
+    if (homePage.style.display === "none") {
+      homePage.style.display = "block";
+    } else {
+      homePage.style.display = "none";
+    }
+  }
